9073ff0626583076cd0efb00a8490e258c63069f
Pravin <pravinmishrag3@gmail.com>
2025-10-05 09:30:59 +0530

    create login page and install the axios and allow the withCredentials true when making api call that will allow to set the cookies in the browser

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ### Git configuration
+
 ##### …or create a new repository on the command line
+
 - git init
 - git add README.md
 - git commit -m "first commit"
@@ -8,11 +10,13 @@
 - git push -u origin main
 
 ##### …or push an existing repository from the command line
+
 - git remote add origin https://github.com/pravin-mishra123/DevTinder-Frontend.git
 - git branch -M main
 - git push -u origin main
 
 ### Episode - 15 || DevTinder UI - part1
+
 - Create a Vite + React Application
 - Remove unecessory code and create a Hello World App
 - install tailwind css => npm install -D tailwindcss postcss autoprefixer npx tailwindcss init -p
@@ -23,3 +27,10 @@
 - Create BrowserRouter > Routes > Route=/ Body > RouteChildren
 - Create an Outlet in your body component
 - Create a footer component
+
+### Episode - 16 || DevTinder UI - part2
+
+- Create a login page
+- install axios
+- CORS - install cors in backend => add middleware to with configuration: origin, credentials:true
+- When making api call in the frontend axios => {withCredentials:true} => that will allow to set the cooking in the browser
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,65 @@
-const Login = () =>{
-    return(
-        <div>Login Component</div>
-    )
-}
+import axios from "axios";
+import { useState } from "react";
 
-export default Login
+const Login = () => {
+  const [emailId, setEmailId] = useState("pravin@gmail.com");
+  const [password, setPassword] = useState("Pravin@123");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:7777/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Email ID</span>
+              </div>
+              <input
+                type="text"
+                value={emailId}
+                // placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </label>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Password{password}</span>
+              </div>
+              <input
+                type="text"
+                value={password}
+                // placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+          </div>
+          <div className="card-actions justify-center m-2">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
